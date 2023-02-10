674a99067e64fda398319c081403f39a16da52af
Peter <jobs.gardeniaoliveira@gmail.com>
2023-02-10 14:50:06 +0000

    added images, changed background and keyup

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+const box = document.querySelector('.box');
+const container = document.querySelector('.container');
 const apiKey = "3ebfe3491b9b8ad4b80a8097bd6c9062";
 const inputSearch = document.querySelector('#input-search');
 const btnSearch = document.querySelector('#btn-search');
 const divWeather = document.querySelector('.weather-info');
 const place = document.querySelector('.location');
 const degree = document.querySelector('.degree');
+const description = document.querySelector('.description');
 const wind = document.querySelector('.wind');
 const humidity = document.querySelector('.humidity');
 const errorMsg = document.querySelector('.error-msg');
@@ -13,17 +16,31 @@ btnSearch.addEventListener("click", () =>{
     const city = inputSearch.value;
     inputSearch.value = "";
     showData(city);
+    imageWeather('snow');
+})
+inputSearch.addEventListener("keyup", (e) =>{
+    if(e.code === "Enter"){
+        divWeather.classList.remove('hidden');
+        const city = e.target.value;
+        inputSearch.value = "";
+        showData(city);
+       
+    }
+   
 
 })
 
 const showData = async(city)=>{
     place.innerText = city;
    const data = await getData(city);
-
+    
    place.innerText = data.name;
+   //description.innerText = data.weather[0].description;
    degree.innerText = parseInt(data.main.temp);
    wind.innerText = data.name;
    humidity.innerText = `${data.main.humidity}%`
+
+   const condition = data.weather[0].description;
    
 }
 
@@ -41,3 +58,28 @@ return data
 
 //});
 }
+
+function imageWeather(condition){
+    if(condition === "snow"){
+        box.style.backgroundImage = `url(img/snow.jpg)`;
+        container.style.color = '#000';
+        inputSearch.style.border = '1px solid #191D3B';
+        
+    } else if(condition === "cloudy"){
+       box.style.backgroundImage = `url(img/cloud.jpg)`;
+       container.style.backgroundColor = '#fff';
+       container.style.color = '#000';
+       inputSearch.style.border = '1px solid #191D3B';
+    }else if(condition === "rain"){
+       box.style.backgroundImage = `url(img/rain.jpg)`;
+       container.style.backgroundColor = '#fff';
+       container.style.color = '#000';
+       inputSearch.style.border = '1px solid #191D3B';
+    }else if(condition === "sun"){
+        box.style.backgroundImage = `url(img/sun.jpg)`;
+        //container.style.backgroundColor = '#f97316';
+        degree.style.color = '#fff';
+    }
+   
+}
+
